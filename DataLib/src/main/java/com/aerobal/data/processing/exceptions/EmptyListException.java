4ba14ffb410847522cd7c8b980499b02767235f6
Jesus E. Luzon Gonzalez <jesus.luzon@upr.edu>
2014-03-09 01:33:07 -0400

    Initial upload. Includes full DataProcessing test suite.

---
+++ DataLib/src/main/java/com/aerobal/data/processing/exceptions/EmptyListException.java
@@ -0,0 +1,26 @@
+package com.aerobal.data.processing.exceptions;
+
+@SuppressWarnings("serial")
+public class EmptyListException extends Exception {
+
+	public EmptyListException() {
+	}
+
+	public EmptyListException(String message) {
+		super(message);
+	}
+
+	public EmptyListException(Throwable cause) {
+		super(cause);
+	}
+
+	public EmptyListException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public EmptyListException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
